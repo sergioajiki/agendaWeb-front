5dbdb701cf09e7357725d937b6c0ef8ed2290538
Sergio Ajiki <sergioajk@hotmail.com>
2025-01-23 15:43:54 -0400

    feat: melhora a captura dos erros do cadastro

--- src/service/userService.ts
+++ src/service/userService.ts
@@ -2,6 +2,11 @@ import { User } from "@/app/models/User";
 import api from "./api";
 
 export const createUser = async (user: User): Promise<any> => {
-    const response = await api.post('/api/users', user);
-    return response.data;
+    try {
+        const response = await api.post('/api/users', user);
+        return response.data;
+    } catch (error: any) {
+        console.error("Erro ao cadastrar usuário!", error.response?.data || error.message);
+        throw new Error("Erro ao cadastrar usuário");
+    }
 };
