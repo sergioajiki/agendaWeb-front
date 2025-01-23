b366542725229de12430b2f6c8cc64144fc1afe9
Sergio Ajiki <sergioajk@hotmail.com>
2025-01-23 02:43:32 -0300

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
         <ol>
           <li>
             Get started by editing <code>src/app/page.tsx</code>.
+            Iniciar frontend com Next.js
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
