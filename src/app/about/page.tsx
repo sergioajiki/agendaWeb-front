7fd9d87ba664b7810fb17e6d483121b593610f56
Sergio Ajiki <sergioajk@hotmail.com>
2025-01-23 14:40:04 -0400

    feat: pagina About utilizando informações do health

---
+++ src/app/about/page.tsx
@@ -0,0 +1,43 @@
+import { healthService } from "@/service/healthService";
+import { HealthData } from "../models/Health";
+
+export default async function AboutPage() {
+    let healthData: HealthData | null = null;
+    try {
+        //Obtém os dados do serviço de health
+        healthData = await healthService() as HealthData;
+    } catch (error) {
+        console.error("Erro ao obter informações de health", error);
+    }
+
+    //Mensagem de erro caso não tenha conseguido obter os dados
+    if (!healthData) {
+        return (
+            <div>
+                <h1>
+                    Erro ao obter informações de health
+                    <p>Não foi possível carregar os dados do serviço de health</p>
+                </h1>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <h1>
+                Sobre o Sistema
+            </h1>
+            <ul>
+                <li><strong>Nome da Aplicação:</strong> {healthData.applicationName}</li>
+                <li><strong>Versão:</strong> {healthData.version}</li>
+                <li><strong>Status:</strong> {healthData.status}</li>
+                <li><strong>Status do Banco de Dados:</strong> {healthData.databaseStatus}</li>
+                <li><strong>Memória Total:</strong> {healthData.totalMemory}</li>
+                <li><strong>Memória Livre:</strong> {healthData.freeMemory}</li>
+                <li><strong>Memória Máxima:</strong> {healthData.maxMemory}</li>
+                <li><strong>Tempo de Atividade:</strong> {healthData.uptime}</li>
+                <li><strong>Timestamp:</strong> {healthData.timestamp}</li>
+            </ul>
+        </div>
+    );
+}
--- src/service/healthService.ts
+++ src/service/healthService.ts
@@ -1,10 +1,10 @@
+import { HealthData } from "@/app/models/Health";
 import api from "./api";
 
-export const healthService = async () => {
-    try{
-
-        const health =  await api.get('/api/health');
-        return health.data;
+export const healthService = async (): Promise<HealthData> => {
+    try {
+        const health = await api.get('/api/health');
+        return health.data as HealthData;
     } catch (error) {
         console.error("Erro ao obter informações de health", error);
         throw new Error("Não foi possível carregar os dados do serviço de health");
